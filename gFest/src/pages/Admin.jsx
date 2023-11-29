07253e4599b9fbbb4c8e6d80b51ee48cb39ae14c
Kalu Daniel <krayzhee419@gmail.com>
2023-11-29 18:28:26 +0100

    feat:added admin page

--- gFest/src/pages/Admin.jsx
+++ gFest/src/pages/Admin.jsx
@@ -1,11 +1,75 @@
+import Nav from "../components/Nav";
+import axios from "axios";
+import { FaUserFriends } from "react-icons/fa";
+import { PiUsersFourFill } from "react-icons/pi";
+import { useState, useEffect } from "react";
+import "../Styles/Admin.css";
+
 const Admin = () => {
-  return ( 
+  const [error, setError] = useState(null);
+  const [data, setData] = useState({});
+
+  async function handler() {
+    try {
+      const res = await axios.get(
+        "https://events-register.onrender.com/api/v1/admin/stats"
+      );
+
+      if (res.status !== 200) {
+        setError(`Request failed with status: ${res.status}`);
+      } else {
+        setData(res.data);
+        console.log(data);
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
+  useEffect(() => {
+    handler();
+  }, []);
+
+  return (
     <div className="admin">
+      <Nav />
       <div className="main-admin">
-        
+        <div className="prt-a">
+          <div className="reg">
+            <div className="reg-a">
+              <FaUserFriends />
+              <p>No of RSVP:</p>
+            </div>
+            <div className="reg-b">{data.numOfRSVP}</div>
+          </div>
+          <div className="att">
+            <div className="reg-a">
+              <PiUsersFourFill />
+              <p>Attendance:</p>
+            </div>
+            <div className="reg-b">{data.numOfAttended}</div>
+          </div>
+        </div>
+        <div className="prt-b">
+          <form>
+            <input type="text" placeholder="*Enter Access Code" required />
+            <button type="submit">Search</button>
+          </form>
+
+          <div className="user">
+            <p>{`Name: Kalu Daniel Obinna`}</p>
+            <p>{`Email: krayzhee419@gmail.com`}</p>
+            <p>{`Phone Number: 08153270969`}</p>
+
+            <button type="submit" className="custom-btn btn-13 btn-3">
+              Confirm
+            </button>
+            {error && <p className="error">{error}</p>}
+          </div>
+        </div>
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Admin;
--- gFest/src/pages/Home.jsx
+++ gFest/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
-import Image1 from "../Imgs/flier.jpg";
+import Image1 from "../Imgs/flier2.jpg";
 import "../Styles/Home.css";
 import { Link } from "react-router-dom";
 import { FiArrowUpRight } from "react-icons/fi";
@@ -12,12 +12,12 @@ const Home = () => {
         <div className="prt-a">
           <h2>GAME FEST IS HERE</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam harum
-            hic voluptatum iure? Autem earum libero voluptatibus facere nisi
-            nobis corporis laudantium, dolorem asperiores quam vero culpa
-            quibusdam beatae adipisci. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Placeat quam porro rerum pariatur maiores vitae,
-            obcaecati reprehenderit assumenda recusandae odit?
+            Get ready for a gaming extravaganza like no other! Its time to level
+            up your fun and RSVP for the Games Fest of the year. Immerse
+            yourself in a day of thrilling adventures, where gamers unite,
+            friendships are forged, and joy knows no bounds. Our Games Fest
+            promises an atmosphere charged with excitement, laughter, and the
+            shared passion for gaming
           </p>
           <Link to="/form" className="btn">
             <p>RSVP</p>
